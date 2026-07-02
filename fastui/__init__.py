8d84bd8fba926d3685bb0b349c036bc9cf2dc718
NEFORCEO <pirogartem919@gmail.com>
2026-07-02 14:52:03 +0700

    feat: create fastui/__init__.py

---
+++ fastui/__init__.py
@@ -0,0 +1,11 @@
+from .__meta__ import __version__
+from .app import App
+from .components import ui
+from . import components
+
+__all__ = (
+    "__version__",
+    "App",
+    "ui",
+    "components",
+)
