ba0757d6f031a9cce23950428eac35e7ed4385ad
NEFORCEO <pirogartem919@gmail.com>
2026-07-02 14:51:49 +0700

    feat: create fastui/openapi/__init__.py

---
+++ fastui/openapi/__init__.py
@@ -0,0 +1,8 @@
+from .generator import generate_openapi_schema
+from .swagger import get_docs_html, get_not_found_html
+
+__all__ = (
+    "generate_openapi_schema",
+    "get_docs_html",
+    "get_not_found_html",
+)
